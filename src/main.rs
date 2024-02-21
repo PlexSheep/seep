76ec945d1e0d7ffd25cc4b26814c80b8a4a61538
Christoph J. Scherr <Christoph.Scherr@newtec.de>
2024-02-21 13:06:09 +0100

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs,
+    io::{self, prelude::*},
+};
+
+fn main() -> Result<(), io::Error> {
+    let mut tty = std::fs::File::options()
+        .write(true)
+        .read(false)
+        .open("/dev/tty")?;
+    io::copy(&mut io::stdin(), &mut tty)?;
+    tty.flush()?;
+    io::copy(&mut io::stdin(), &mut io::stdout())?;
+    Ok(())
 }
